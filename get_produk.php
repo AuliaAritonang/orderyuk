2f4ea1e3b3b0fb0f764f22275784eb67b459a5c6
AuliaAritonang <aritonangaulia@gmail.com>
2020-10-23 10:28:13 +0700

    update

--- get_produk.php
+++ get_produk.php
@@ -5,5 +5,5 @@
 
 	$sql = mysqli_query($db,"SELECT * FROM produk WHERE id_produk='".$id_produk."'");
 
-	echo json_encode(mysqli_fetch_array($sql));
+	return json_encode(mysqli_fetch_array($sql));
 ?>
